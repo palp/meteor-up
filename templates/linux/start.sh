6a91a3da9a614ae5e7a985ccd66dd2797ecc64eb
palp <palp@palp.org>
2015-09-30 07:47:37 -0700

    Use new image

--- templates/linux/start.sh
+++ templates/linux/start.sh
@@ -32,7 +32,7 @@ if [ "$USE_LOCAL_MONGO" == "1" ]; then
     --hostname="$HOSTNAME-$APPNAME" \
     --env=MONGO_URL=mongodb://mongodb:27017/$APPNAME \
     --name=$APPNAME \
-    meteorhacks/meteord:base
+    meteorhacks/meteord:app
 else
   docker run \
     -d \
@@ -42,7 +42,7 @@ else
     --hostname="$HOSTNAME-$APPNAME" \
     --env-file=$ENV_FILE \
     --name=$APPNAME \
-    meteorhacks/meteord:base
+    meteorhacks/meteord:app
 fi
 
 <% if(typeof sslConfig === "object")  { %>
